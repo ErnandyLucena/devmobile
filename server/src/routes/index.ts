56f7837fee445d26a13f8be2a69c4ea7e60c2f6c
Gabriela Borba <gborbas.gb@gmail.com>
2025-10-24 14:12:10 -0300

    Get médicos e funcionários

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes";
+import usersRoutes from "./users.routes";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/", usersRoutes);
 
 export default router;
